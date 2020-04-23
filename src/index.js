5f57c9e75ee4d99a63a40741af8ba3e079b38b1c
Jeffrey Lui <jlui820@gmail.com>
2020-04-22 18:34:01 -0700

    work on chart

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-
 import { graph } from './graph';
 import { circle } from './circle';
+// import { line } from './line'
 
 document.addEventListener("DOMContentLoaded", () => {
     graph();
     circle();
+    // line();
 });
